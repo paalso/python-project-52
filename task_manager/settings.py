60d5e6ec559201a456a70d2e12edb3915e34a60c
Pavel Soroka <40a.pvl@gmail.com>
2025-06-19 16:10:15 +0300

    WIP: templates

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'task_manager'
 ]
 
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
