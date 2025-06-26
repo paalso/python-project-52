d62905763ed464a2f75c580c94048387059b1979
Pavel Soroka <40a.pvl@gmail.com>
2025-06-26 20:09:39 +0300

    WIP: users list template

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,3 +1,4 @@
+# task_manager/users/urls.py
 from django.urls import path
 
 from . import views
@@ -5,5 +6,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.UserListView.as_view(), name='index'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,16 +1,36 @@
+# task_manager/users/views.py
 from django.shortcuts import render
 from django.views import View
 
 from task_manager.users.models import CustomUser
 
 
-class IndexView(View):
+class UserListView(View):
     def get(self, request, *args, **kwargs):
-        template_name = 'users/list.html'
         users = CustomUser.objects.all()
         print(users)
         return render(
             request,
-            template_name,
+            'users/list.html',
             context={'users': users}
+        )
+
+
+class UserUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = CustomUser.objects.get(id=user_id)
+        # form = UserForm(instance=user)
+        return render(
+            request, 'users/update.html'
+        )
+
+
+class UserDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = CustomUser.objects.get(id=user_id)
+        # form = UserForm(instance=user)
+        return render(
+            request, 'users/delete.html'
         )
