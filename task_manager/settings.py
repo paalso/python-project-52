59eb833a4add8b02db0e391581eb9ece4dc7c796
Pavel Soroka <40a.pvl@gmail.com>
2025-06-30 13:04:13 +0300

    Add django-crispy-forms, crispy-bootstrap5 dependancies

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'task_manager',
     'task_manager.users',
 ]
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
