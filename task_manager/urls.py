6ad770d339a9c7bf8f60145bc3dfb2751d5e192e
Pavel Soroka <40a.pvl@gmail.com>
2025-07-01 14:50:06 +0300

    UPD index page implementation

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from task_manager import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('users/', include('task_manager.users.urls')),
     path('admin/', admin.site.urls),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,13 @@
 import logging
 
 from django.shortcuts import render
+from django.views import View
 
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    user = request.user
-    logger.info(f'User: {user}, authenticated: {user.is_authenticated}')
-    return render(request, 'index.html')
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        logger.info(f'User: {user}, authenticated: {user.is_authenticated}')
+        return render(request, 'index.html')
