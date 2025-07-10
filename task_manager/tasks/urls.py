4f10268ff5eda3f6153a7346c75a2f8975a31c31
Pavel Soroka <40a.pvl@gmail.com>
2025-07-10 20:32:52 +0300

    Impement tasks.views.TaskListView and its template

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -7,4 +7,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='list'),
+    path('create/', views.TaskCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.TaksUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='delete'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,16 +1,32 @@
 import logging
 
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import (
-    ListView,
+    ListView, CreateView, UpdateView, DeleteView
 )
 
 # from .forms import StatusForm
-# from .models import Status
+from .models import Task
 
 logger = logging.getLogger(__name__)
 
 
 class TaskListView(ListView):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Task list must be implemented here')
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'tasks/list.html'
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    pass
+
+
+class TaksUpdateView(LoginRequiredMixin, UpdateView):
+    pass
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    pass
