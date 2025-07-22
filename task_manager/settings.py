d0f9f189481a5877740eece854dd9eea29d379f1
Pavel Soroka <40a.pvl@gmail.com>
2025-07-22 17:29:40 +0300

    UPD the project settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -1,21 +1,35 @@
 # ruff: noqa: E501
 
+import os
 from pathlib import Path
+
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bm+o*c=v23gq!=uh5m8iy*sqsn60z!npy1$#f29ociw$h485vn'
+load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == "True"
 
-ALLOWED_HOSTS = ['webserver', 'localhost', '127.0.0.1']
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(",")
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
+}
 
 # User model
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -73,17 +87,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -1,3 +1,5 @@
+# ruff: noqa: E402
+
 """
 WSGI config for task_manager project.
 
@@ -8,9 +10,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+import dotenv
+
+# Load .env file before Django settings
+project_dir = Path(__file__).resolve().parent.parent
+dotenv.load_dotenv(dotenv_path=project_dir / ".env")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
