dd47dd32ad2d9efc1387cbbddd9dab58fbaada2a
Pavel Soroka <40a.pvl@gmail.com>
2025-07-22 20:27:56 +0300

    UPD task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,12 +23,15 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(",")
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+default_db_config = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+
+if not default_db_config['ENGINE'].endswith('sqlite3') and not DEBUG:
+    default_db_config['OPTIONS'] = {'sslmode': 'require'}
+
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
+    'default': default_db_config
 }
 
 # User model
