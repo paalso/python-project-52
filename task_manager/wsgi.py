a58450189c9ea6b1c9a1ec2f43c469aa84d4cc69
Pavel Soroka <40a.pvl@gmail.com>
2025-07-24 14:07:12 +0300

    UPD wsgi.py

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -1,24 +1,16 @@
 # ruff: noqa: E402
 
-"""
-WSGI config for task_manager project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 from pathlib import Path
 
-import dotenv
+project_folder = Path(__file__).resolve().parent.parent / 'python-project-52'
 
-# Load .env file before Django settings
-project_dir = Path(__file__).resolve().parent.parent
-dotenv.load_dotenv(dotenv_path=project_dir / ".env")
+path = '/home/paalso/python-project-52/'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'task_manager.settings'
 
 from django.core.wsgi import get_wsgi_application
 
