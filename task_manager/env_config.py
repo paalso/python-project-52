a9d5304c4a0edcc2f2bb0392876793bca58d4c35
Pavel Soroka <40a.pvl@gmail.com>
2025-07-24 16:16:47 +0300

    UPD task_manager/env_config.py

--- task_manager/env_config.py
+++ task_manager/env_config.py
@@ -3,14 +3,23 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
+DEFAULT_HOST = 'webserver'
+
 
 def load_env(base_dir):
     load_dotenv(dotenv_path=base_dir / ".env")
 
+    allowed_hosts = os.getenv('ALLOWED_HOSTS', '')
+    allowed_hosts = [host.strip() for host in allowed_hosts.split(',')
+                     if host.strip()]
+
+    if DEFAULT_HOST not in allowed_hosts:
+        allowed_hosts.append(DEFAULT_HOST)
+
     return {
         'DEBUG': os.getenv('DEBUG', 'False') == "True",
         'SECRET_KEY': os.getenv('SECRET_KEY'),
-        'ALLOWED_HOSTS': os.getenv('ALLOWED_HOSTS', '').split(','),
+        'ALLOWED_HOSTS': allowed_hosts,
         'DATABASES': {
             'default': _build_db_config()
         }
